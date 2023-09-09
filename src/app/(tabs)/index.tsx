a1d2f65f8abf4f79f3c341aac2fecfeebdd1a40b
Faysal Bsata <faysalbsata@Faysals-MacBook-Pro.local>
2023-09-09 13:31:09 +0400

    recommendation query

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,7 +1,39 @@
-import { FlatList } from 'react-native';
-import { tracks } from '../../../assets/data/tracks';
+import { ActivityIndicator, FlatList } from 'react-native';
 import TrackListItem from '../../components/TrackListItem';
+import { gql, useQuery } from '@apollo/client';
+import { Text } from '../../components/Themed';
+const query = gql`
+  query MyQuery($genres: String!) {
+    recommendations(seed_genres: $genres) {
+      tracks {
+        id
+        name
+        preview_url
+        artists {
+          id
+          name
+        }
+        album {
+          id
+          name
+          images {
+            width
+            url
+            height
+          }
+        }
+      }
+    }
+  }
+`;
 export default function HomeScreen() {
+  const { data, loading, error } = useQuery(query, {
+    variables: { genres: 'drum-and-bass,house' },
+  });
+
+  if (loading) return <ActivityIndicator />;
+  if (error) return <Text>{`Error! ${error.message}`}</Text>;
+  const tracks = data?.recommendations?.tracks || [];
   return (
     <FlatList
       data={tracks}
