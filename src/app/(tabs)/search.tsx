3d38019cba0a6146e675aed4b6f44d3e86bb7f7c
Faysal Bsata <faysalbsata@Faysals-MacBook-Pro.local>
2023-09-09 13:37:13 +0400

    search query

--- src/app/(tabs)/search.tsx
+++ src/app/(tabs)/search.tsx
@@ -1,12 +1,48 @@
-import { FlatList, TextInput, View, StyleSheet } from 'react-native';
-import { tracks } from '../../../assets/data/tracks';
+import {
+  FlatList,
+  TextInput,
+  View,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
 import TrackListItem from '../../components/TrackListItem';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useState } from 'react';
 import { FontAwesome } from '@expo/vector-icons';
 import { Text } from '../../components/Themed';
+import { gql, useQuery } from '@apollo/client';
+const query = gql`
+  query MyQuery($q: String!) {
+    search(q: $q) {
+      tracks {
+        items {
+          id
+          name
+          preview_url
+          artists {
+            id
+            name
+          }
+          album {
+            id
+            name
+            images {
+              width
+              url
+              height
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 export default function SearchScreen() {
   const [search, setSearch] = useState('');
+  const { data, loading, error } = useQuery(query, {
+    variables: { q: search },
+  });
+  const tracks = data?.search?.tracks?.items || [];
   return (
     <SafeAreaView>
       <View style={styles.header}>
@@ -19,6 +55,8 @@ export default function SearchScreen() {
         />
         <Text onPress={() => setSearch('')}>Cancel</Text>
       </View>
+      {loading && <ActivityIndicator />}
+      {error && <Text>{error.message}</Text>}
       <FlatList
         data={tracks}
         renderItem={({ item }) => <TrackListItem track={item} />}
